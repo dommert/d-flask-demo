86515b752f9614d5ad4d40adc1bea51a926f1d18
Dommert <DommertEnterprises@Gmail.com>
2014-06-23 23:29:01 -0700

    routing and templates
    testing

--- flaskapp/main.py
+++ flaskapp/main.py
@@ -1,6 +1,7 @@
 # Dommert's Flask Foundation
 from flask import Flask, render_template
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route('/')
 def hello_world(title='Hello World'):
@@ -9,6 +10,15 @@ def hello_world(title='Hello World'):
 @app.route('/about')
 def about(title='About Page'):
     return render_template('foundation/onecol.html', title=title)
+
+
+@app.route('/example')
+def example(title='Example Page'):
+    return render_template('foundation/example.html', title=title)
+
+@app.route('/test')
+def test(title='Test Page'):
+    return render_template('theme/about.html', title=title)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
